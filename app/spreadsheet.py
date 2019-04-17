a7d0798ac7731a34a0fe019cee25a6d2d8c0331d
Colin Mills <cgm71@georgetown.edu>
2019-04-17 00:32:54 -0400

    updated config so heroku can use

--- app/spreadsheet.py
+++ app/spreadsheet.py
@@ -20,16 +20,16 @@ load_dotenv()
 DOCUMENT_KEY = os.environ.get("GOOGLE_SHEET_ID", "OOPS Please get the spreadsheet identifier from its URL")
 SHEET_NAME = "Form Responses 1"
 
-CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "client_secret.json")
-#GOOGLE_API_CREDENTIALS = os.environ.get("GOOGLE_API_CREDENTIALS")
+#CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "client_secret.json")
+GOOGLE_API_CREDENTIALS = os.environ.get("GOOGLE_API_CREDENTIALS")
 AUTH_SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
     "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
 ]
 
 def get_products():
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
-    #credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
+    #credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+    credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
     client = gspread.authorize(credentials) #> <class 'gspread.client.Client'>
     #print("GETTING PRODUCTS FROM THE SPREADSHEET...")
     doc = client.open_by_key(DOCUMENT_KEY) #> <class 'gspread.models.Spreadsheet'>
