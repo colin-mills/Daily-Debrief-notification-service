8cfac4566041d0d6354868862383b7bd1f4ce34f
Colin Mills <cgm71@georgetown.edu>
2019-04-21 13:43:01 -0400

    Better message formats

--- app/send_notification.py
+++ app/send_notification.py
@@ -1,10 +1,4 @@
 # adapted from https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/sendgrid.md
-
-#import json
-#import requests
-#import datetime
-#import statistics
-
 from source.Alpha import GetStockInfo
 from source.NYTimes import GetNYTArticles
 from spreadsheet import get_products
@@ -16,33 +10,32 @@ headers, contactList = get_products()
 for contact in contactList:
 
     if contact["How would you like to be contacted?"] == "Email":
-
         name = contact["Name"]
         contact_email = contact["Email"]
+        #Defaults for enail template
         stockInfo = "No stock information chosen"
         newsInfo = "No news information chosen"
         weatherInfo = "No weather information chosen"
         sportsInfo = "No sports information chosen"
         musicInfo = "No music information chosen"
 
-
         if contact["Would you like stock information?"] == "Yes":
             for stock in range(int(contact["How many stocks would you like?"])):
                 stockTicker = contact["Stock " + str(stock + 1)]
-                stockInfo = stockInfo + GetStockInfo(stockTicker, 1)
+                stockInfo = stockInfo + "<br>" + GetStockInfo(stockTicker, 1)
 
             #passes along relevant information to be sent
         
         if contact["Would you like news information?"] == "Yes":
             for article in range(int(contact["How many articles would you like to see?"])):
-                newsInfo = GetNYTArticles(article, 1)
+                newsInfo = newsInfo + GetNYTArticles(article, 1)
 
         if contact["Would you like weather information?"] == "Yes":
-            print("weather info in here")
+            weatherInfo = "weather info in here"
         if contact["Would you like sports information?"] == "Yes":
-            print("sports info in here")
+            sportsInfo = "sports info in here"
         if contact["Would you like music information?"] == "Yes":
-            print("music info in here")
+            musicInfo = "music info in here"
 
         
         send_email(name, contact_email, stockInfo, newsInfo, weatherInfo, sportsInfo, musicInfo)
@@ -51,21 +44,35 @@ for contact in contactList:
         number = "+" + str(contact["Phone number"])
         content = ""
 
-        if contact["Would you like stock information?"] == "Yes":
-             for stock in range(int(contact["How many stocks would you like?"])):
-                stockTicker = contact["Stock " + str(stock + 1)]
-                content = content + GetStockInfo(stockTicker)
+        #Text messages seperated by type of info
+        stockInfo = "No stock information chosen"
+        newsInfo = "No news information chosen"
+        weatherInfo = "No weather information chosen"
+        sportsInfo = "No sports information chosen"
+        musicInfo = "No music information chosen"
+
+        if contact["Would you like weather information?"] == "Yes":
+            weatherInfo = "weather info in here"
+            send_text(number, weatherInfo)
         
         if contact["Would you like news information?"] == "Yes":
             for article in range(int(contact["How many articles would you like to see?"])):
-                content = content + GetNYTArticles(article)
+                newsInfo = newsInfo + GetNYTArticles(article)
+            send_text(number, newsInfo)
 
-        if contact["Would you like weather information?"] == "Yes":
-            print("weather info in here")
+        if contact["Would you like stock information?"] == "Yes":
+             for stock in range(int(contact["How many stocks would you like?"])):
+                stockTicker = contact["Stock " + str(stock + 1)]
+                stockInfo = stockInfo + GetStockInfo(stockTicker)
+
+             send_text(number, stockInfo)
+
         if contact["Would you like sports information?"] == "Yes":
-            print("sports info in here")
+            sportsInfo = "sports info in here"
+            send_text(number, sportsInfo)
         if contact["Would you like music information?"] == "Yes":
-            print("music info in here")
+            musicInfo = "music info in here"
+            send_text(number, musicInfo)
        
         #passes along relevant information to be sent
         send_text(number, content)
@@ -73,26 +80,36 @@ for contact in contactList:
     elif contact["How would you like to be contacted?"] == "Twitter":
         Handle = "@" + str(contact["Twitter"])
 
-        if contact["Would you like stock information?"] == "Yes":
-             for stock in range(int(contact["How many stocks would you like?"])):
-                stockTicker = contact["Stock " + str(stock + 1)]
-                message = GetStockInfo(stockTicker)
-                send_tweet(Handle, message)
+        #Tweets seperated by type of info
+        stockInfo = "No stock information chosen"
+        newsInfo = "No news information chosen"
+        weatherInfo = "No weather information chosen"
+        sportsInfo = "No sports information chosen"
+        musicInfo = "No music information chosen"
+
+        if contact["Would you like weather information?"] == "Yes":
+            weatherInfo = "weather info in here"
+            send_tweet(Handle, weatherInfo)
 
         if contact["Would you like news information?"] == "Yes":
             for article in range(int(contact["How many articles would you like to see?"])):
-                message = GetNYTArticles(article)
-                send_tweet(Handle, message)
+                newsInfo = GetNYTArticles(article)
+                send_tweet(Handle, newsInfo)
 
-        if contact["Would you like weather information?"] == "Yes":
-            print("weather info in here")
+        if contact["Would you like stock information?"] == "Yes":
+             for stock in range(int(contact["How many stocks would you like?"])):
+                stockTicker = contact["Stock " + str(stock + 1)]
+                stockInfo =  stockInfo + GetStockInfo(stockTicker)
+                
+             send_tweet(Handle, stockInfo)
+
         if contact["Would you like sports information?"] == "Yes":
-            print("sports info in here")
-        if contact["Would you like music information?"] == "Yes":
-            print("music info in here")
+            sportsInfo ="sports info in here"
+            send_tweet(Handle, sportsInfo)
 
-        #passes along relevant information to be sent
-        send_tweet(Handle, message)
+        if contact["Would you like music information?"] == "Yes":
+            musicInfo = "music info in here"
+            send_tweet(Handle, musicInfo)
 
 if __name__ == "__main__":
     print("headers:", headers)
