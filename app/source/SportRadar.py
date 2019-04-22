6b11c08433997e9291b5f852697d89e59fceb37c
Colin Mills <cgm71@georgetown.edu>
2019-04-22 13:31:37 -0400

    Email interface update

--- app/source/SportRadar.py
+++ app/source/SportRadar.py
@@ -29,14 +29,22 @@ def getSportsInfo(sportLeague, email = 0):
         API_KEY = os.environ.get("NFL_API_KEY")
         request_url = "http://api.sportradar.us/nfl-t1/teams/{}/rankings.json?api_key={}".format(Season,API_KEY)
         response = requests.get(request_url)
-        print(response)
+        #print(response)
 
         parsed_response = json.loads(response.text)
 
-        for conference in parsed_response["conferences"]:
-            message_text = message_text + conference["name"] + ": \n"
-            for division in conference["divisions"]:
-                message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
+        if email == 1:
+            message_text = "Rankings for the " + sport + " " + Season + " season: <br>"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": <br>"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")<br>"
+        else:
+            message_text = "Rankings for the " + sport + " " + Season + " season: \n"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": \n"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
 
     elif sport == "NBA":
         
@@ -45,20 +53,28 @@ def getSportsInfo(sportLeague, email = 0):
         else:
             Season = str(Date.year)
 
-        print(Season)
+        #print(Season)
 
         API_KEY = os.environ.get("NBA_API_KEY")
         request_url = "http://api.sportradar.us/nba/trial/v5/en/seasons/{}/REG/rankings.json?api_key={}".format(Season,API_KEY)
 
         response = requests.get(request_url)
-        print(response)
+        #print(response)
 
         parsed_response = json.loads(response.text)
 
-        for conference in parsed_response["conferences"]:
-            message_text = message_text + conference["name"] + ": \n"
-            for division in conference["divisions"]:
-                message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
+        if email == 1:
+            message_text = "Rankings for the " + sport + " " + Season + " season: <br>"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": <br>"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")<br>"
+        else:
+            message_text = "Rankings for the " + sport + " " + Season + " season: \n"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": \n"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
                 
     elif sport == "NHL":
 
@@ -71,55 +87,58 @@ def getSportsInfo(sportLeague, email = 0):
         request_url = "http://api.sportradar.us/nhl/trial/v6/en/seasons/{}/REG/rankings.json?api_key={}".format(Season,API_KEY)
         #print(request_url)
         response = requests.get(request_url)
-        print(response)
+        #print(response)
 
         parsed_response = json.loads(response.text)
 
-        for conference in parsed_response["conferences"]:
-            message_text = message_text + conference["name"] + ": \n"
-            for division in conference["divisions"]:
-                message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
-                message_text =  message_text + "The number two team in the " + division["name"] + " is the " + division["teams"][1]["name"] + " (conference: " + str(division["teams"][1]["rank"]["conference"]) + ")\n"
+        if email == 1:
+            message_text = "Rankings for the " + sport + " " + Season + " season: <br>"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": <br>"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")<br>"
+                    message_text =  message_text + "The number two team in the " + division["name"] + " is the " + division["teams"][1]["name"] + " (conference: " + str(division["teams"][1]["rank"]["conference"]) + ")<br>"
+        else:
+            message_text = "Rankings for the " + sport + " " + Season + " season: \n"
+            for conference in parsed_response["conferences"]:
+                message_text = message_text + conference["name"] + ": \n"
+                for division in conference["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (conference: " + str(division["teams"][0]["rank"]["conference"]) + ")\n"
+                    message_text =  message_text + "The number two team in the " + division["name"] + " is the " + division["teams"][1]["name"] + " (conference: " + str(division["teams"][1]["rank"]["conference"]) + ")\n"
     elif sport == "MLB":
 
         if Date.month < 3:
             Season = str(int(Date.year) - 1)
         else:
             Season = str(Date.year)
-        print(Season)
+        #print(Season)
 
         #print(NFL_Season)
         API_KEY = os.environ.get("MLB_API_KEY")
         request_url = "http://api.sportradar.us/mlb/trial/v6.5/en/seasons/{}/REG/rankings.json?api_key={}".format(Season,API_KEY)
         #print(request_url)
         response = requests.get(request_url)
-        print(response)
+        #print(response)
 
         parsed_response = json.loads(response.text)
-        print(parsed_response.keys())
+        #print(parsed_response.keys())
 
         
         #message_text = message_text + parsed_response["league"]["name"] + ": \n"
-
-        for league in parsed_response["league"]["season"]["leagues"]:
-            message_text = message_text + league["name"] + ": \n"
-            for division in league["divisions"]:
-                message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (league: " + str(division["teams"][0]["rank"]["league"]) + ")\n"     
+        if email == 1:
+            message_text = "Rankings for the " + sport + " " + Season + " season: <br>"
+            for league in parsed_response["league"]["season"]["leagues"]:
+                message_text = message_text + league["name"] + ": <br>"
+                for division in league["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (league: " + str(division["teams"][0]["rank"]["league"]) + ")<br>"     
+        else:
+            message_text = "Rankings for the " + sport + " " + Season + " season: \n"
+            for league in parsed_response["league"]["season"]["leagues"]:
+                message_text = message_text + league["name"] + ": \n"
+                for division in league["divisions"]:
+                    message_text =  message_text + "The number one team in the " + division["name"] + " is the " + division["teams"][0]["name"] + " (league: " + str(division["teams"][0]["rank"]["league"]) + ")\n"     
 
     return message_text
-
-    
-
-
-
-    #request_url = "api.openweathermap.org/data/2.5/forecast/daily?zip={},{}&appid={}".format(zipCode,country,API_KEY)
-    ##https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=b1b15e88fa797225412429c1c50c122a1
-    ##parses this data from json to dict
-    #response = requests.get(request_url)
-    #print(response)
-    #parsed_response = json.loads(response.text)
-#
-    #print(parsed_response)
 
 if __name__ == "__main__":
     message = getSportsInfo("NFL")
