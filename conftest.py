ae61a53c19052306d9ab6f67d4da7f2d007e785f
Colin Mills <cgm71@georgetown.edu>
2019-04-23 11:24:32 -0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,1 @@
-from app.source.Alpha import GetStockInfo
-from app.source.NYTimes import GetNYTArticles
-from app.source.OpenWeatherMap import getWeatherInfo
-from app.source.SportRadar import getSportsInfo
+
